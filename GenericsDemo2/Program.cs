0d4719bb0286abd5680546779e35039999b5d23d
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-16 13:05:21 +0530

    [PALLAVI] Add: ExtendMaxMethod

--- GenericsDemo2/Program.cs
+++ GenericsDemo2/Program.cs
@@ -13,24 +13,28 @@
             //string Result2 = MaximumString.FindMaxString("Apple", "Peach", "Banana");
             //Console.WriteLine("Maximum String is:" + Result2);
 
-            GenericsMaximum<int> obj = new GenericsMaximum<int>(23, 50, 5);
-            int result = obj.FindMaxUsingGenerics(23, 50, 5);
-            Console.WriteLine("Maximum Integer is:" + result);
-            Console.WriteLine(result);
+            //GenericsMaximum<int> obj = new GenericsMaximum<int>(23, 50, 5);
+            //int result = obj.FindMaxUsingGenerics(23, 50, 5);
+            //Console.WriteLine("Maximum Integer is:" + result);
+            //Console.WriteLine(result);
 
 
 
-            GenericsMaximum<float> obj1 = new GenericsMaximum<float>(23.25f, 50.25f, 50.3f);
-            float result2 = obj1.FindMaxUsingGenerics(23.25f, 50.25f, 50.3f);
-            Console.WriteLine("Maximum Float is :" + result2);
-            Console.WriteLine(result2);
-           
+            //GenericsMaximum<float> obj1 = new GenericsMaximum<float>(23.25f, 50.25f, 50.3f);
+            //float result2 = obj1.FindMaxUsingGenerics(23.25f, 50.25f, 50.3f);
+            //Console.WriteLine("Maximum Float is :" + result2);
+            //Console.WriteLine(result2);
 
-            GenericsMaximum<string> obj2 = new GenericsMaximum<string>("Apple", "Peach", "Banana");
-            string result3 = obj2.FindMaxUsingGenerics("Apple", "Peach", "Banana");
-            Console.WriteLine("Greatest String is :" + result3);
-            Console.WriteLine(result3);
 
+            //GenericsMaximum<string> obj2 = new GenericsMaximum<string>("Apple", "Peach", "Banana");
+            //string result3 = obj2.FindMaxUsingGenerics("Apple", "Peach", "Banana");
+            //Console.WriteLine("Greatest String is :" + result3);
+            //Console.WriteLine(result3);
+
+            //extend max method for more than theree parameter
+            ExtendMaxMethod<string> max = new ExtendMaxMethod<string>("Apple", "Peach", "Banana", "Mango");
+            string maxString = max.GetMax();
+            Console.WriteLine(maxString);
 
 
 
